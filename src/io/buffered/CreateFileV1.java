7300aa9466ba6d5aa9445463aa7ae3b9144266b3
tngh4037 <tngh4037@gmail.com>
2024-10-11 16:00:15 +0900

    section2: I/O 기본1 - 파일 입출력과 성능 최적화1 - 하나씩 쓰기

---
+++ src/io/buffered/CreateFileV1.java
@@ -0,0 +1,22 @@
+package io.buffered;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CreateFileV1 {
+
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream(BufferedConst.FILE_NAME);
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < BufferedConst.FILE_SIZE; i++) { // 10MB 크기의 파일 생성
+            fos.write(1);
+        }
+        fos.close();
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("File created: " + BufferedConst.FILE_NAME);
+        System.out.println("File size: " + BufferedConst.FILE_SIZE / 1024 / 1024 + "MB");
+        System.out.println("Time taken: " + (endTime - startTime) + "ms"); // 89초 걸림...! ( 10MB 만드는데 이렇게 오래걸린다. )
+    }
+}
