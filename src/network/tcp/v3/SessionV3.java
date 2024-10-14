036870c28990a36d309c83744c845fe771fcbf41
tngh4037 <tngh4037@gmail.com>
2024-10-15 01:11:19 +0900

    section6: 네트워크 - 프로그램1 - 네트워크 프로그램3

---
+++ src/network/tcp/v3/SessionV3.java
@@ -0,0 +1,51 @@
+package network.tcp.v3;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import static util.MyLogger.log;
+
+// (소켓을 통해) 특정 클라이언트와 통신하기 위한 역할을 하는 객체
+public class SessionV3 implements Runnable {
+
+    private final Socket socket;
+
+    public SessionV3(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+            while (true) {
+                // 클라이언트로 부터 문자 받기
+                String received = input.readUTF(); // 블로킹
+                log("client -> server: " + received);
+
+                if (received.equals("exit")) {
+                    break;
+                }
+
+                // 클라이언트에게 문자 보내기
+                String toSend = received + " World!";
+                output.writeUTF(toSend);
+                log("client <- server: " + toSend);
+            }
+
+            // 자원 정리
+            log("연결 종료: " + socket);
+            input.close();
+            output.close();
+            socket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
