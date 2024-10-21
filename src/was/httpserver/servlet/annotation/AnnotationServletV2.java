4f8fc9a11d203a474bc7182d12419e6ff7b95137
tngh4037 <tngh4037@gmail.com>
2024-10-22 05:26:54 +0900

    section12: HTTP 서버 활용 - HTTP 서버8 - 애노테이션 서블릿2 - 동적 바인딩

---
+++ src/was/httpserver/servlet/annotation/AnnotationServletV2.java
@@ -0,0 +1,66 @@
+package was.httpserver.servlet.annotation;
+
+import was.httpserver.HttpRequest;
+import was.httpserver.HttpResponse;
+import was.httpserver.HttpServlet;
+import was.httpserver.PageNotFoundException;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+// 애노테이션 기반의 컨트롤러를 처리할 수 있는 서블릿
+// 리팩토링: 컨트롤러에서 사용하지 않는 파라미터까지 받지 않도록. ( 딱 필요한 것만 받을 수 있도록 )
+public class AnnotationServletV2 implements HttpServlet {
+
+    private final List<Object> controllers;
+
+    public AnnotationServletV2(List<Object> controllers) {
+        this.controllers = controllers;
+    }
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws IOException {
+        String path = request.getPath();
+
+        for (Object controller : controllers) {
+            Class<?> aClass = controller.getClass();
+            Method[] methods = aClass.getDeclaredMethods();
+            for (Method method : methods) {
+
+                if (method.isAnnotationPresent(Mapping.class)) {
+                    Mapping annotation = method.getAnnotation(Mapping.class);
+                    String value = annotation.value();
+                    if (value.equals(path)) {
+                        invoke(controller, method, request, response);
+                        return;
+                    }
+                }
+            }
+        }
+
+        throw new PageNotFoundException("request = " + path);
+    }
+
+    private static void invoke(Object controller, Method method, HttpRequest request, HttpResponse response) {
+        Class<?>[] parameterTypes = method.getParameterTypes();
+        Object[] args = new Object[parameterTypes.length];
+
+        for (int i = 0; i < parameterTypes.length; i++) {
+            if (parameterTypes[i] == HttpRequest.class) {
+                args[i] = request;
+            } else if (parameterTypes[i] == HttpResponse.class) {
+                args[i] = response;
+            } else {
+                throw new IllegalArgumentException("Unsupported parameter type: " + parameterTypes[i]);
+            }
+        }
+
+        try {
+            method.invoke(controller, args);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
